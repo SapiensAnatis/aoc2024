0df97089d55c8814352e2e16586c4ceefb01936f
Jay Malhotra <jay0@me.com>
2024-12-31 01:29:20 +0000

    Inline grid methods to header file

--- lib/grid.cpp
+++ lib/grid.cpp
@@ -29,101 +29,6 @@ std::ostream &operator<<(std::ostream &stream, Direction direction) {
     return stream << text;
 }
 
-Point::Point(int x, int y) : x(x), y(y) {}
-
-Point::Point() : Point(0, 0) {}
-
-Vector Vector::operator-() const { return {-this->dx, -this->dy}; }
-
-std::unique_ptr<Grid> Grid::create(std::vector<char> squares, int width) {
-    return std::make_unique<Grid>(std::move(squares), width);
-}
-
-std::unique_ptr<Grid> Grid::create(char fill, int width, int height) {
-    std::vector<char> squares(width * height, fill);
-    return Grid::create(squares, width);
-}
-
-Grid::Grid(std::vector<char> squares, int width) : width(width), squares(std::move(squares)) {
-    this->height = static_cast<int>(this->squares.size()) / this->width;
-}
-
-int Grid::get_width() const { return this->width; }
-
-int Grid::get_height() const { return this->height; }
-
-std::optional<char> Grid::get_square(int x, int y) const {
-    if (x < 0 || x >= this->width) {
-        return std::nullopt;
-    }
-
-    if (y < 0 || y >= this->height) {
-        return std::nullopt;
-    }
-
-    return get_square_unsafe(x, y);
-}
-
-std::optional<char> Grid::get_square(Point point) const {
-    return this->get_square(point.x, point.y);
-}
-
-char Grid::get_square_unsafe(int x, int y) const {
-    auto index = this->calculate_array_index(x, y);
-
-    aoc_assert(index < this->squares.size(), "Grid bounds check failure");
-
-    return this->squares[index];
-}
-
-char Grid::get_square_unsafe(Point point) const {
-    return this->get_square_unsafe(point.x, point.y);
-}
-
-std::unique_ptr<Grid> Grid::with_mutation(int x, int y, char new_value) {
-    auto new_vec = this->squares;
-    new_vec[calculate_array_index(x, y)] = new_value;
-    return std::make_unique<Grid>(new_vec, this->width);
-}
-
-void Grid::set_square(int x, int y, char new_value) {
-    auto index = this->calculate_array_index(x, y);
-    aoc_assert(index > 0 && index < this->squares.size(), "Grid bounds check failure");
-
-    this->squares[index] = new_value;
-}
-
-void Grid::set_square(Point point, char new_value) {
-    auto index = this->calculate_array_index(point.x, point.y);
-    aoc_assert(index < this->squares.size(), "Grid bounds check failure");
-
-    this->squares[index] = new_value;
-}
-
-std::vector<Point> Grid::get_adjacent_points(Point point) const {
-    Point north = {point.x, point.y - 1};
-    Point east = {point.x + 1, point.y};
-    Point south = {point.x, point.y + 1};
-    Point west = {point.x - 1, point.y};
-
-    std::vector<Point> vec;
-
-    if (this->get_square(north)) {
-        vec.push_back(north);
-    }
-    if (this->get_square(east)) {
-        vec.push_back(east);
-    }
-    if (this->get_square(south)) {
-        vec.push_back(south);
-    }
-    if (this->get_square(west)) {
-        vec.push_back(west);
-    }
-
-    return vec;
-}
-
 std::ostream &operator<<(std::ostream &stream, const Grid &grid) {
     std::string result;
     result.reserve(grid.squares.size() + grid.height);
@@ -136,15 +41,6 @@ std::ostream &operator<<(std::ostream &stream, const Grid &grid) {
     }
 
     return stream << result;
-}
-
-std::vector<char>::size_type Grid::calculate_array_index(int x, int y) const {
-    aoc_assert(x >= 0 && x < this->width && y >= 0 && y < this->height,
-               "grid bounds check failure");
-
-    int offset = y * this->width;
-    int index = offset + x;
-    return index;
 }
 
 Grid::Iterator Grid::begin() { return Grid::Iterator(this->squares.begin()); }
@@ -189,21 +85,9 @@ bool operator!=(const Grid::Iterator &a, const Grid::Iterator &b) {
     return a.vec_iterator != b.vec_iterator;
 }
 
-bool Point::operator==(Point other) const { return other.x == this->x && other.y == this->y; }
-
 std::ostream &operator<<(std::ostream &stream, Point point) {
     return stream << "(" << point.x << ", " << point.y << ")";
 }
-
-Point operator+(Point point, Vector vector) { return {point.x + vector.dx, point.y + vector.dy}; }
-
-Point operator-(Point point, Vector vector) { return {point.x - vector.dx, point.y - vector.dy}; }
-
-Vector operator-(Point a, Point b) { return {a.x - b.x, a.y - b.y}; }
-
-Vector operator/(Vector a, int b) { return {a.dx / b, a.dy / b}; }
-
-Vector operator*(Vector a, int b) { return {a.dx * b, a.dy * b}; }
 
 std::unique_ptr<Grid> parse_grid(std::istream &input) {
     std::vector<char> grid;
@@ -222,30 +106,4 @@ std::unique_ptr<Grid> parse_grid(std::istream &input) {
     return Grid::create(std::move(grid), width);
 }
 
-bool operator==(Vector a, Vector b) { return a.dx == b.dx && a.dy == b.dy; }
-
-bool operator!=(Vector a, Vector b) { return !(a == b); }
-
 } // namespace aoc
-
-namespace std {
-
-size_t hash<aoc::Point>::operator()(aoc::Point p) const {
-    size_t seed = 0;
-
-    aoc::hash_combine(seed, hash<int>()(p.x));
-    aoc::hash_combine(seed, hash<int>()(p.y));
-
-    return seed;
-}
-
-size_t hash<aoc::Vector>::operator()(aoc::Vector v) const {
-    size_t seed = 0;
-
-    aoc::hash_combine(seed, hash<int>()(v.dx));
-    aoc::hash_combine(seed, hash<int>()(v.dy));
-
-    return seed;
-}
-
-} // namespace std
--- lib/grid.h
+++ lib/grid.h
@@ -1,6 +1,9 @@
 #ifndef AOC_2024_GRID_H
 #define AOC_2024_GRID_H
 
+#include "./assert.h"
+#include "hash.hpp"
+
 #include <array>
 #include <iterator>
 #include <memory>
@@ -9,12 +12,20 @@
 
 namespace aoc {
 
-struct Point;
+struct Point {
+    int x;
+    int y;
+
+    Point() : Point(0, 0) {}
+    Point(int x, int y) : x(x), y(y) {}
+
+    bool operator==(Point other) const { return other.x == this->x && other.y == this->y; }
+};
 
 enum class Direction { North, East, South, West };
 
-constexpr std::array<Direction, 4> directions = {Direction::North, Direction::East,
-                                                 Direction::South, Direction::West};
+constexpr std::array directions = {Direction::North, Direction::East, Direction::South,
+                                   Direction::West};
 
 std::ostream &operator<<(std::ostream &stream, Direction direction);
 
@@ -58,7 +69,7 @@ struct Vector {
         return {new_x, new_y};
     }
 
-    Vector operator-() const;
+    Vector operator-() const { return {-this->dx, -this->dy}; }
 };
 
 constexpr std::array<Vector, 4> direction_vectors = {
@@ -67,21 +78,86 @@ constexpr std::array<Vector, 4> direction_vectors = {
 
 class Grid {
   public:
-    static std::unique_ptr<Grid> create(std::vector<char> squares, int width);
-    static std::unique_ptr<Grid> create(char fill, int width, int height);
+    static std::unique_ptr<Grid> create(std::vector<char> squares, int width) {
+        return std::make_unique<Grid>(std::move(squares), width);
+    }
 
-    Grid(std::vector<char> squares, int width);
+    static std::unique_ptr<Grid> create(char fill, int width, int height) {
+        std::vector squares(width * height, fill);
+        return create(squares, width);
+    }
 
-    [[nodiscard]] std::optional<char> get_square(int x, int y) const;
-    [[nodiscard]] std::optional<char> get_square(Point point) const;
-    [[nodiscard]] char get_square_unsafe(int x, int y) const;
-    [[nodiscard]] char get_square_unsafe(Point point) const;
-    [[nodiscard]] int get_width() const;
-    [[nodiscard]] int get_height() const;
-    [[nodiscard]] std::unique_ptr<Grid> with_mutation(int x, int y, char new_value);
-    void set_square(int x, int y, char new_value);
-    void set_square(Point point, char new_value);
-    [[nodiscard]] std::vector<Point> get_adjacent_points(Point point) const;
+    Grid(std::vector<char> squares, int width) : width(width), squares(std::move(squares)) {
+        this->height = static_cast<int>(this->squares.size()) / this->width;
+    }
+
+    [[nodiscard]] std::optional<char> get_square(Point point) const {
+        return this->get_square(point.x, point.y);
+    }
+
+    [[nodiscard]] std::optional<char> get_square(int x, int y) const {
+        if (x < 0 || x >= this->width) {
+            return std::nullopt;
+        }
+
+        if (y < 0 || y >= this->height) {
+            return std::nullopt;
+        }
+
+        return get_square_unsafe(x, y);
+    }
+
+    [[nodiscard]] char get_square_unsafe(Point point) const {
+        return this->get_square_unsafe(point.x, point.y);
+    }
+
+    [[nodiscard]] char get_square_unsafe(int x, int y) const {
+        auto index = this->calculate_array_index(x, y);
+
+        return this->squares[index];
+    }
+
+    [[nodiscard]] int get_width() const { return this->width; }
+    [[nodiscard]] int get_height() const { return this->height; }
+
+    [[nodiscard]] std::unique_ptr<Grid> with_mutation(int x, int y, char new_value) {
+        auto new_vec = this->squares;
+        new_vec[calculate_array_index(x, y)] = new_value;
+        return std::make_unique<Grid>(new_vec, this->width);
+    }
+
+    void set_square(Point point, char new_value) { this->set_square(point.x, point.y, new_value); }
+
+    void set_square(int x, int y, char new_value) {
+        auto index = this->calculate_array_index(x, y);
+        aoc_assert(index > 0 && index < this->squares.size(), "Grid bounds check failure");
+
+        this->squares[index] = new_value;
+    }
+
+    [[nodiscard]] std::vector<Point> get_adjacent_points(Point point) const {
+        Point north = {point.x, point.y - 1};
+        Point east = {point.x + 1, point.y};
+        Point south = {point.x, point.y + 1};
+        Point west = {point.x - 1, point.y};
+
+        std::vector<Point> vec;
+
+        if (this->get_square(north)) {
+            vec.push_back(north);
+        }
+        if (this->get_square(east)) {
+            vec.push_back(east);
+        }
+        if (this->get_square(south)) {
+            vec.push_back(south);
+        }
+        if (this->get_square(west)) {
+            vec.push_back(west);
+        }
+
+        return vec;
+    }
 
     friend std::ostream &operator<<(std::ostream &stream, const Grid &grid);
 
@@ -115,30 +191,38 @@ class Grid {
     Point get_point(const Iterator &iterator);
 
   private:
-    [[nodiscard]] std::vector<char>::size_type calculate_array_index(int x, int y) const;
+    [[nodiscard]] std::vector<char>::size_type calculate_array_index(int x, int y) const {
+        aoc_assert(x >= 0 && x < this->width && y >= 0 && y < this->height,
+                   "grid bounds check failure");
+
+        int offset = y * this->width;
+        int index = offset + x;
+        return index;
+    }
 
     int width;
     int height;
     std::vector<char> squares;
 };
 
-struct Point {
-    int x;
-    int y;
+inline Point operator+(Point point, Vector vector) {
+    return {point.x + vector.dx, point.y + vector.dy};
+}
 
-    Point();
-    Point(int x, int y);
+inline Point operator-(Point point, Vector vector) {
+    return {point.x - vector.dx, point.y - vector.dy};
+}
 
-    bool operator==(Point other) const;
-};
+inline Vector operator-(Point a, Point b) { return {a.x - b.x, a.y - b.y}; }
 
-Point operator+(Point point, Vector vector);
-Point operator-(Point point, Vector vector);
-Vector operator-(Point a, Point b);
-Vector operator/(Vector a, int b);
-Vector operator*(Vector a, int b);
-bool operator==(Vector a, Vector b);
-bool operator!=(Vector a, Vector b);
+inline Vector operator/(Vector a, int b) { return {a.dx / b, a.dy / b}; }
+
+inline Vector operator*(Vector a, int b) { return {a.dx * b, a.dy * b}; }
+
+inline bool operator==(Vector a, Vector b) { return a.dx == b.dx && a.dy == b.dy; }
+
+inline bool operator!=(Vector a, Vector b) { return !(a == b); }
+
 std::ostream &operator<<(std::ostream &stream, Point point);
 std::ostream &operator<<(std::ostream &stream, const Grid &grid);
 
@@ -149,11 +233,25 @@ std::unique_ptr<Grid> parse_grid(std::istream &input);
 namespace std {
 
 template <> struct hash<aoc::Point> {
-    size_t operator()(aoc::Point p) const;
+    size_t operator()(aoc::Point p) const noexcept {
+        size_t seed = 0;
+
+        aoc::hash_combine(seed, hash<int>()(p.x));
+        aoc::hash_combine(seed, hash<int>()(p.y));
+
+        return seed;
+    }
 };
 
 template <> struct hash<aoc::Vector> {
-    size_t operator()(aoc::Vector v) const;
+    size_t operator()(aoc::Vector v) const noexcept {
+        size_t seed = 0;
+
+        aoc::hash_combine(seed, hash<int>()(v.dx));
+        aoc::hash_combine(seed, hash<int>()(v.dy));
+
+        return seed;
+    }
 };
 
 } // namespace std
